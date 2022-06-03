3f82190f375d5e20e641b5b57ee0a8932b12bc29
Javi <javi.richards.a@gmail.com>
2022-06-02 21:44:20 -0400

    Creado app.js

---
+++ pagina/functions/app.js
@@ -0,0 +1,19 @@
+const platillos = [
+    {
+        "id": "Platillo 1",
+        "precio": 10,
+    },
+    {
+        "id": "Platillo 2",
+        "precio": 20,
+    },
+    {
+        "id": "Platillo 3",
+        "precio": 30,
+    },
+    {
+        "id": "Platillo 4",
+        "precio": 40,
+    }
+];
+
